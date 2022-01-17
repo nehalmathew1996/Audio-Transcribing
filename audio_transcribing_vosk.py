0662d95fcf8f5fa3cc8855a66ac0a800c6d4eeb9
Nehal Mathew Alex <82964954+nehalmathew1996@users.noreply.github.com>
2022-01-17 14:55:40 +0530

    Update audio_transcribing_vosk.py

--- audio_transcribing_vosk.py
+++ audio_transcribing_vosk.py
@@ -1,11 +1,3 @@
-import argparse
-import io
-from google.oauth2 import service_account
-#from pydub import AudioSegment
-import os 
-from google.cloud import speech
-# from google.cloud.speech import enums
-from google.cloud.speech_v1 import types  
 import os
 import sys
 import wave
@@ -13,62 +5,7 @@ import json
 
 from vosk import Model, KaldiRecognizer, SetLogLevel
 
-#import Word
-
-import wave
-
-def audio_transcribe_gtts(speech_file):
-    
-    credentials = service_account.Credentials.from_service_account_file('api-key-new.json')
-    dict1=dict()
-    print("Start")
-    
-    #speech_file='/content/drive/MyDrive/stt and time stamp/out0.wav'
-    language='en'
-
-    print("checking credentials")
-        
-    client = speech.SpeechClient(credentials=credentials)
-
-
-    with io.open(speech_file, 'rb') as audio_file:
-        content = audio_file.read()
-                
-    print("audio file read")    
-        
-    audio = speech.RecognitionAudio(content=content)
-
-
-
-    
-    a = wave.open(speech_file,"rb")
-    print('Frame Rate:',a.getframerate())      
-    
-    
-    print("config start")
-    # return 0
-    # config = speech.RecognitionConfig(encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,language_code=language,enable_word_time_offsets=True)
-    config = speech.RecognitionConfig(encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-                                    language_code=language,
-                                    enable_word_time_offsets=True,
-                                    enable_automatic_punctuation=True,
-                                    max_alternatives=1)
-
-    print("Recognizing:")
-    response = client.recognize(config=config, audio=audio) 
-    print("Recognized")
-
-    # streaming_config = speech.StreamingRecognitionConfig(
-    #         config=config, interim_results=True)
-
-    for result in response.results:
-        #print('inside')
-        alternative = result.alternatives[0]
-        print('Transcript: {}'.format(alternative.transcript))
-        transcript=alternative.transcript
-
-
-
+# Call fuction sent path to speech file as parameter
 def audio_transcribe_vosk(speech_file):
 
     # path to vosk model downloaded from
